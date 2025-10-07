ead23fc37d4e10c1ff42b10d32baaa511eb16a92
vxrm <126362292+vxrm@users.noreply.github.com>
2025-10-07 18:03:23 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,37 +1,11 @@
-
-local cloneref = cloneref or function(obj)
-    return obj
-end
-
-local Players = cloneref(game:GetService('Players'))
-local starterPlayer = cloneref(game:GetService('StarterPlayer'))
-local inputService = cloneref(game:GetService('UserInputService'))
-local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
-local runService = cloneref(game:GetService('RunService'))
-local starterGui = cloneref(game:GetService('StarterGui'))
-
-local Camera = workspace.CurrentCamera
-local LocalPlayer = Players.LocalPlayer
-
-if runService:IsStudio() then
-    if not shared then
-        getfenv().shared = getfenv()
-    end
-end
-
 local ids = {
-    [17750024818] = '17750024818.lua',
-    [122483927964273] = '122483927964273.lua',
-    [71480482338212] = '71480482338212.lua',
-    [6872274481] = '6872274481.lua',
-    [8444591321] = '6872274481.lua',
-    [8560631822] = '6872274481.lua'
+    [71480482338212] = 'BedwarZ.lua'
 }
 
 local function downloadFile(file)
     url = file:gsub('catware/', '')
     if not isfile(file) then
-        writefile(file, game:HttpGet('https://raw.githubusercontent.com/catwareroblox/catware'..readfile('catware/commit.txt')..'/'..url))
+        writefile(file, game:HttpGet('https://raw.githubusercontent.com/catwareroblox/catware/'..readfile('catware/commit.txt')..'/'..url))
     end
 
     repeat task.wait() until isfile(file)
