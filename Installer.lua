da30ac0a20c14820f706b5df9ce1376b8c1421d3
vxrm <126362292+vxrm@users.noreply.github.com>
2025-10-08 18:33:43 -0400

    Create Installer.lua

---
+++ Installer.lua
@@ -0,0 +1,50 @@
+local base = "https://raw.githubusercontent.com/catwareroblox/catware/refs/heads/main/"
+
+local function getDownload(file)
+    file = file:gsub('idk/', '')
+
+    local suc, ret = pcall(function()
+        return game:HttpGet(base .. file)
+    end)
+
+    return suc and ret or 'print("Failed to get ' .. file..'")'
+end
+
+local function downloadFile(file)
+    file = 'idk/' .. file
+
+    if not isfile(file) then
+        writefile(file, getDownload(file))
+    end
+
+    repeat task.wait() until isfile(file)
+
+    return readfile(file)
+end
+
+local function debugDownloadSuccess(file)
+    local File = downloadFile(file)
+
+    if isfile('idk/' .. file) then
+        print('[idk]: Successfully downloaded', file)
+    else
+        print('[idk]: Failed to download', file)
+    end
+
+    return File
+end
+
+for i,v in {'idk', 'idk/games', 'idk/configs', 'libraries'} do
+    if not isfolder(v) then
+        makefolder(v)
+    end
+end
+
+debugDownloadSuccess('GuiLibrary.lua')
+
+local Games = {'bedwarZ', 'bedwars'}
+for i,v in Games do
+    debugDownloadSuccess('Games/'..v..'.lua')
+end
+
+return loadstring(debugDownloadSuccess('Main.lua'))()
